e0c3b89319d4c1ae59db41fa4b89779f07003e50
Faraj Farook <farajfarook@gmail.com>
2019-06-09 01:01:27 +1000

    Adding discuss

--- discuss/discuss.module.ts
+++ discuss/discuss.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { DiscussService } from './services/discuss.service';
-import { DiscussComponent } from './components/discuss.component';
+import { PostComponent } from './components/post.component';
 import { CommonModule } from '@angular/common';
-import { MatCardModule, MatInputModule, MatButtonModule, MatFormFieldModule, MatDialogModule } from '@angular/material';
+import { MatCardModule, MatInputModule, MatButtonModule, MatFormFieldModule, MatDialogModule, MatIconModule } from '@angular/material';
 import { PostCreateComponent } from './components/post-create.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { FlexLayoutModule } from '@angular/flex-layout';
@@ -16,14 +16,15 @@ import { FlexLayoutModule } from '@angular/flex-layout';
         MatCardModule,
         MatInputModule,
         MatDialogModule,
+        MatIconModule,
         MatFormFieldModule,
         MatButtonModule
     ],
     declarations: [
         PostCreateComponent,
-        DiscussComponent
+        PostComponent
     ],
-    exports: [DiscussComponent],
+    exports: [PostComponent],
     providers: [DiscussService],
     entryComponents: [PostCreateComponent]
 })
